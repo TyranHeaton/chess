9c399814d063bcf9054b857f946fcb2b243c00c1
Tyran Heaton <tyranheaton@gmail.com>
2026-01-11 16:10:36 -0700

    All 3 tests now pass for the ChessPosition module.

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -1,17 +1,20 @@
 package chess;
 
+import java.util.Objects;
+
 /**
  * Represents a single square position on a chess board
  * <p>
  * Note: You can add to this class, but you may not alter
- * signature of the existing methods.
+ * the signature of the existing methods.
  */
 public class ChessPosition {
-    private int row;
-    private int col;
+    private final int row;
+    private final int col;
 
     public ChessPosition(int row, int col) {
-
+        this.row = row;
+        this.col = col;
     }
 
     /**
@@ -19,15 +22,28 @@ public class ChessPosition {
      * 1 codes for the bottom row
      */
     public int getRow() {
-        return this.row;
+        return row;
     }
 
     /**
      * @return which column this position is in
      * 1 codes for the left row
      */
-    public int getColumn() {
-        return this.col;
+    public int getColumn() { return col; }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(row, col);
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessPosition that = (ChessPosition) o;
+        return row == that.row && col == that.col;
+    }
 }
+
+
